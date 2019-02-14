d2ad3106de25cd06b21f82ffdb78bcc0309e66e6
TheGameFreak720 <luis.alvarez@commithub.com>
2019-02-13 23:14:43 -0500

    separated tests between auth routes and managing notes routes

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -4,113 +4,120 @@ const db = require('../database/dbConfig');
 
 
 describe('The route handlers', () => {
-    describe('Post /note/create', () => {
 
-        afterEach(async () => {
-            await db('notes').truncate();
-            await db.seed.run();
-        });
+    describe('Auth routes', () => {
 
-        it('responds with 201 if body is correct', async () => {
-            const body = {title: 'Coding is fun', description: 'Diandra made me do TDD. SOS', user_id: 3}
-            const response = await request(server).post('/note/create').send(body);
-
-            expect(response.status).toBe(201);
-            db('notes').truncate();
-        });
-
-        it('responds with the new note id', async () => {
-            const body = {title: 'Coding is fun', description: 'Diandra made me do TDD. SOS', user_id: 3}
-            const response = await request(server).post('/note/create').send(body);
-
-            expect(response.body[0]).toBeGreaterThan(0);
-            db('notes').truncate();
-        });
-
-        it('responds with 401 when body is missing data', async () => {
-            const body = { }
-            const response = await request(server).post('/note/create').send(body);
-
-            expect(response.status).toBe(401);
-            db('notes').truncate();
-        });
     });
 
-    describe('Get /note/get/all', () => {
-        it('responds with 200', async () => {
-            const response = await request(server).get('/note/get/all');
+    describe('Managing note routes', () => {
+        describe('Post /note/create', () => {
 
-            expect(response.status).toBe(200);
+            afterEach(async () => {
+                await db('notes').truncate();
+                await db.seed.run();
+            });
+
+            it('responds with 201 if body is correct', async () => {
+                const body = {title: 'Coding is fun', description: 'Diandra made me do TDD. SOS', user_id: 3}
+                const response = await request(server).post('/note/create').send(body);
+
+                expect(response.status).toBe(201);
+                db('notes').truncate();
+            });
+
+            it('responds with the new note id', async () => {
+                const body = {title: 'Coding is fun', description: 'Diandra made me do TDD. SOS', user_id: 3}
+                const response = await request(server).post('/note/create').send(body);
+
+                expect(response.body[0]).toBeGreaterThan(0);
+                db('notes').truncate();
+            });
+
+            it('responds with 401 when body is missing data', async () => {
+                const body = { }
+                const response = await request(server).post('/note/create').send(body);
+
+                expect(response.status).toBe(401);
+                db('notes').truncate();
+            });
         });
 
-        it('responds with an object', async () => {
-            const response = await request(server).get('/note/get/all');
+        describe('Get /note/get/all', () => {
+            it('responds with 200', async () => {
+                const response = await request(server).get('/note/get/all');
 
-            expect(typeof response.body).toBe('object');
-        });
-    });
+                expect(response.status).toBe(200);
+            });
 
-    describe('Put /note/edit/:id', () => {
-        afterEach(async () => {
-            await db('notes').truncate();
-            await db.seed.run();
-        });
+            it('responds with an object', async () => {
+                const response = await request(server).get('/note/get/all');
 
-        it('responds with 200', async () => {
-            const body = {title: 'Coding is fun', description: 'Diandra made me do TDD. SOS', user_id: 3};
-            body.id = 3;
-
-            const response = await request(server).put('/note/edit/3').send(body);
-
-            expect(response.status).toBe(200);
-            db('notes').truncate();
+                expect(typeof response.body).toBe('object');
+            });
         });
 
-        it('responds with an object', async () => {
-            const body = {title: 'Coding is fun', description: 'Diandra made me do TDD. SOS', user_id: 3};
-            body.id = 3;
+        describe('Put /note/edit/:id', () => {
+            afterEach(async () => {
+                await db('notes').truncate();
+                await db.seed.run();
+            });
 
-            const response = await request(server).put('/note/edit/3').send(body);
+            it('responds with 200', async () => {
+                const body = {title: 'Coding is fun', description: 'Diandra made me do TDD. SOS', user_id: 3};
+                body.id = 3;
 
-            expect(typeof response.body).toBe('object');
-            db('notes').truncate();
+                const response = await request(server).put('/note/edit/3').send(body);
+
+                expect(response.status).toBe(200);
+                db('notes').truncate();
+            });
+
+            it('responds with an object', async () => {
+                const body = {title: 'Coding is fun', description: 'Diandra made me do TDD. SOS', user_id: 3};
+                body.id = 3;
+
+                const response = await request(server).put('/note/edit/3').send(body);
+
+                expect(typeof response.body).toBe('object');
+                db('notes').truncate();
+            });
+
+            it('responds with 401 if body is missing data', async () => {
+                const body = { }
+
+                const response = await request(server).put('/note/edit/3').send(body);
+
+                expect(response.status).toBe(401);
+                db('notes').truncate();
+            });
         });
 
-        it('responds with 401 if body is missing data', async () => {
-            const body = { }
+        describe('Delete note/delete/:id', () => {
+            afterEach(async () => {
+                await db('notes').truncate();
+                await db.seed.run();
+            });
 
-            const response = await request(server).put('/note/edit/3').send(body);
+            it('responds with 200', async () => {
+                const response = await request(server).delete('/note/delete/1');
 
-            expect(response.status).toBe(401);
-            db('notes').truncate();
-        });
-    });
+                expect(response.status).toBe(200);
+                db('notes').truncate();
+            });
 
-    describe('Delete note/delete/:id', () => {
-        afterEach(async () => {
-            await db('notes').truncate();
-            await db.seed.run();
-        });
+            it('responds with the number of records removed', async () => {
+                const response = await request(server).delete('/note/delete/1');
 
-        it('responds with 200', async () => {
-            const response = await request(server).delete('/note/delete/1');
+                expect(response.body).toBe(1);
+                db('notes').truncate();
+            });
 
-            expect(response.status).toBe(200);
-            db('notes').truncate();
-        });
+            it('responds with 404 when params have missing data', async () => {
+                const params = undefined;
+                const response = await request(server).delete(`/note/delete/${params}`);
 
-        it('responds with the number of records removed', async () => {
-            const response = await request(server).delete('/note/delete/1');
-
-            expect(response.body).toBe(1);
-            db('notes').truncate();
-        });
-
-        it('responds with 404 when params have missing data', async () => {
-            const params = undefined;
-            const response = await request(server).delete(`/note/delete/${params}`);
-
-            expect(response.status).toBe(404);
+                expect(response.status).toBe(404);
+            });
         });
     });
 });

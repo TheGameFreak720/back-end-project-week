79ce12ee387ad13a584fc0bf4b1c1fd6d363cad8
TheGameFreak720 <luis.alvarez@commithub.com>
2019-02-13 22:03:10 -0500

    configure expressed session and dotenv

--- api/server.js
+++ api/server.js
@@ -1,16 +1,32 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const session = require('express-session');
 
 const configureRoutes = require('../config/routes.js');
 
+dotenv.config()
+
 const server = express();
+
+const sess = {
+    secret: process.env.EXPRESS_SESSION_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true
+};
+
+if(server.get('env') === 'production') {
+    sess.cookie.secure = true;
+}
 
 server.use(helmet());
 server.use(cors());
 server.use(morgan('dev'));
 server.use(express.json());
+server.use(session(sess));
 
 configureRoutes(server);
 

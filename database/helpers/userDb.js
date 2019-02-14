8256c4ee150a15f92544b497bece1555600a9558
TheGameFreak720 <luis.alvarez@commithub.com>
2019-02-13 23:40:18 -0500

    added get helper to userDb

--- database/helpers/userDb.js
+++ database/helpers/userDb.js
@@ -4,4 +4,8 @@ module.exports = {
     add: function(user) {
         return db('users').insert(user);
     },
+
+    get: function(user) {
+        return db('users').where('username', user.username)
+    },
 }

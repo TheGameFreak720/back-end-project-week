cfbf75d8c817134697f735d82cd048be67ed99a4
TheGameFreak720 <luis.alvarez@commithub.com>
2019-02-13 23:44:23 -0500

    created login function

--- config/routes.js
+++ config/routes.js
@@ -30,6 +30,26 @@ const register = async (req, res) => {
     }
 };
 
+const login = (req, res) => {
+    const user = req.body;
+
+    userDB.get(user)
+        .then(users => {
+            if(users.length && bcrypt.compareSync(user.password, users[0].password)) {
+                const token = jwt.sign({ id: users[0].id }, process.env.JWT_SECRET, {
+                expiresIn: 86400 // expires in 24 hours
+                });
+                
+                res.json({ message: 'Logged in', jwt: token });
+            } else {
+                res.status(404).json({ message: 'login information is incorrect' });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ errorMessage: 'Failed to verify. Please try again' });
+        });
+};
+
 
 //Managing note funtions
 const addNote = async (req, res) => {
